cc1ef46905f6e38fe60df9f76615dac08cb6b092
Rishabh Jain <jainrishabh8153@gmail.com>
2025-07-20 23:24:29 +0530

    Project

---
+++ backend/src/service/storage.service.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var ImageKit = require("imagekit");
+
+var imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+
+function uploadFile(file) {
+    return new Promise((resolve, reject) => {
+         imagekit.upload({
+            file: file.buffer, // required
+            fileName:new mongoose.Types.ObjectId().toString(), // required
+            folder:"moodsongs",
+         },(error, result) => {
+            if (error) {
+             reject(error);
+            }else{
+                resolve(result);
+            }
+         })
+    });
+}
+
+module.exports = uploadFile;
